9117e9eac72a66cfc64d3af181bd0c98e2d482bc
Costea Adrian Nicolae <an.costea.2015@gmail.com>
2021-10-15 05:16:23 +0300

    void-packages v94.0.4606.81

--- ungoogled-chromium/files/chromium.sh
+++ ungoogled-chromium/files/chromium.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export CHROME_WRAPPER=/usr/lib/ungoogled-chromium/ungoogled-chromium
-export CHROME_DESKTOP=ungoogled-chromium.desktop
+export CHROME_WRAPPER=/usr/lib/chromium/chromium
+export CHROME_DESKTOP=chromium.desktop
 CHROME_FLAGS="--enable-gpu-rasterization $CHROME_FLAGS"
-exec /usr/lib/ungoogled-chromium/ungoogled-chromium $CHROME_FLAGS "$@"
+exec /usr/lib/chromium/chromium $CHROME_FLAGS "$@"
