eae6e6a65b3e749a3155eb2823ce4ac4672bcbd5
Costea Adrian-Nicolae <an.costea.2015@gmail.com>
2024-09-22 02:57:52 +0300

    129.0.6668.58

--- void-packages/srcpkgs/ungoogled-chromium/files/chromium.sh
+++ void-packages/srcpkgs/ungoogled-chromium/files/chromium.sh
@@ -2,4 +2,7 @@
 export CHROME_WRAPPER=/usr/lib/ungoogled-chromium/ungoogled-chromium
 export CHROME_DESKTOP=ungoogled-chromium.desktop
 CHROME_FLAGS="--enable-gpu-rasterization $CHROME_FLAGS"
-exec /usr/lib/ungoogled-chromium/ungoogled-chromium $CHROME_FLAGS "$@"
+case $(xbps-uhelper arch) in
+	*-musl) exec /usr/lib/ungoogled-chromium/ungoogled-chromium $CHROME_FLAGS --js-flags="--jitless --wasm_jitless" "$@";;
+	*) exec /usr/lib/ungoogled-chromium/ungoogled-chromium $CHROME_FLAGS "$@";;
+esac
